277db2d4a361b1fcf75e507ad4f864c09094b48c
Jguer <joaogg3@gmail.com>
2016-08-22 03:28:10 +0100

    Fixed styling

--- driver/controller.go
+++ driver/controller.go
@@ -1,112 +1,112 @@
 package driver
 
 import (
-    "github.com/tarm/serial"
-    keybd "github.com/micmonay/keybd_event"
-    "log"
-    "bufio"
-    "time"
-    "strconv"
-    "sync"
+	"bufio"
+	keybd "github.com/micmonay/keybd_event"
+	"github.com/tarm/serial"
+	"log"
+	"strconv"
+	"sync"
+	"time"
 )
 
-const CHALLENGE string = "Hi. Who are you?"
-const REPEAT_DELAY time.Duration = 160 //Milliseconds
-const SLEEP_TIME time.Duration = 1500 //Milliseconds
+const challenge string = "Hi. Who are you?"
+const repeatDelay time.Duration = 160 //Milliseconds
+const sleepTime time.Duration = 1500  //Milliseconds
 
 // CreateController creates a new serial connection to a device
 // and handles all communication and key interpretation
-func CreateController (device string, kb_array [8]int, wg *sync.WaitGroup) {
-    c := &serial.Config{Name: device, Baud: 9600}
-    s, err := serial.OpenPort(c)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer s.Close()
-    // Creates Keyboard
-    kb,err := keybd.NewKeyBonding()
-    if err!=nil {
-        log.Fatal(err)
-    }
-    // Arduino and Keyboard Setup time
-    time.Sleep(SLEEP_TIME * time.Millisecond)
+func CreateController(device string, kbArray [8]int, wg *sync.WaitGroup) {
+	c := &serial.Config{Name: device, Baud: 9600}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer s.Close()
+	// Creates Keyboard
+	kb, err := keybd.NewKeyBonding()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Arduino and Keyboard Setup time
+	time.Sleep(sleepTime * time.Millisecond)
 
-    n, err := s.Write([]byte(CHALLENGE))
-    if err != nil {
-        log.Fatal(err)
-    }
+	n, err := s.Write([]byte(challenge))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    buf := make([]byte, 128)
-    n, err = s.Read(buf)
-    if err != nil {
-        log.Fatal(err)
-    } else if n == 0 {
-        log.Fatal("No response from HES")
-    }
+	buf := make([]byte, 128)
+	n, err = s.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	} else if n == 0 {
+		log.Fatal("No response from HES")
+	}
 
-    log.Println("Communication established with " + device)
-    reader := bufio.NewReader(s)
+	log.Println("Communication established with " + device)
+	reader := bufio.NewReader(s)
 
-    //Each HES key has it's on channel to signal completion
-    var signal [8]chan bool
-    index   := 0
-    counter := 0
+	//Each HES key has it's on channel to signal completion
+	var signal [8]chan bool
+	index := 0
+	counter := 0
 
-    for i := range signal {
-        signal[i] = make(chan bool)
-    }
+	for i := range signal {
+		signal[i] = make(chan bool)
+	}
 
-    for {
-        read_key, err := reader.ReadBytes('\n')
-        if err != nil {
-            panic(err)
-        }
+	for {
+		readKey, err := reader.ReadBytes('\n')
+		if err != nil {
+			panic(err)
+		}
 
-        if read_key[0] == 'P' {
-            // log.Println("Pressed")
-            index, _ = strconv.Atoi(string(read_key[1]))
+		if readKey[0] == 'P' {
+			// log.Println("Pressed")
+			index, _ = strconv.Atoi(string(readKey[1]))
 
-            //Pressing Select 5 Times closes the controller
-            if index == 0 {
-                counter ++
-                if counter == 5 {
-                    wg.Done()
-                    return
-                }
-            } else {
-                counter = 0
-            }
-            go sendKeys(signal[index], read_key[1], kb, kb_array)
-        } else if read_key[0] == 'R' {
-            index, _ = strconv.Atoi(string(read_key[1]))
-            signal[index] <-true
-            // log.Println("Released")
-        }
-        // log.Println(string(read_key))
-    }
+			//Pressing Select 5 Times closes the controller
+			if index == 0 {
+				counter++
+				if counter == 5 {
+					wg.Done()
+					return
+				}
+			} else {
+				counter = 0
+			}
+			go sendKeys(signal[index], readKey[1], kb, kbArray)
+		} else if readKey[0] == 'R' {
+			index, _ = strconv.Atoi(string(readKey[1]))
+			signal[index] <- true
+			// log.Println("Released")
+		}
+		// log.Println(string(read_key))
+	}
 }
 
 // getKey converts the received key to a keyboard key code
-func getKey(key byte, kb_array [8]int) int {
-    x, _ := strconv.Atoi(string(key))
-    return kb_array[x]
+func getKey(key byte, kbArray [8]int) int {
+	x, _ := strconv.Atoi(string(key))
+	return kbArray[x]
 }
 
 // sendKeys handles sending keystrokes to host system
-func sendKeys(signal chan bool, key byte, kb keybd.KeyBonding, kb_array [8]int) {
-    kb.SetKeys(getKey(key, kb_array)) //set keys
-    var err error
-    for {
-        select {
-        case <-signal:
-            return
-        default:
-            // log.Println("Sent a keystroke " + string(key))
-            err = kb.Launching() //launch
-            if err!=nil {
-                panic(err)
-            }
-            time.Sleep(REPEAT_DELAY * time.Millisecond)
-        }
-    }
+func sendKeys(signal chan bool, key byte, kb keybd.KeyBonding, kbArray [8]int) {
+	kb.SetKeys(getKey(key, kbArray)) //set keys
+	var err error
+	for {
+		select {
+		case <-signal:
+			return
+		default:
+			// log.Println("Sent a keystroke " + string(key))
+			err = kb.Launching() //launch
+			if err != nil {
+				panic(err)
+			}
+			time.Sleep(repeatDelay * time.Millisecond)
+		}
+	}
 }
--- hes.go
+++ hes.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-    "io/ioutil"
-    "strings"
-    "github.com/Jguer/go-hes/driver"
-    "log"
-    "errors"
 	"encoding/json"
-    "sync"
-    keybd "github.com/micmonay/keybd_event"
+	"errors"
+	"github.com/Jguer/go-hes/driver"
+	keybd "github.com/micmonay/keybd_event"
+	"io/ioutil"
+	"log"
+	"strings"
+	"sync"
 )
 
 type keybinding struct {
 	Selk  string `json:"select"`
 	Start string `json:"start"`
 	Up    string `json:"up"`
-    Down  string `json:"down"`
-    Left  string `json:"left"`
-    Right string `json:"right"`
-    A_key string `json:"a"`
-    B_key string `json:"b"`
+	Down  string `json:"down"`
+	Left  string `json:"left"`
+	Right string `json:"right"`
+	AKey  string `json:"a"`
+	BKey  string `json:"b"`
 }
 
 // findArduino looks for the file that represents the Arduino
@@ -28,118 +28,117 @@ type keybinding struct {
 // Arduino, otherwise an empty string if unable to find
 // something that 'looks' like an Arduino device.
 func findArduino() ([]string, int, error) {
-    contents, _ := ioutil.ReadDir("/dev")
-    n := 0
-    var duinos []string
-    // Look for what is mostly likely the Arduino device
-    for _, f := range contents {
-        if strings.Contains(f.Name(), "tty.usbserial") ||
-        strings.Contains(f.Name(), "ttyUSB") {
-            duinos = append(duinos, "/dev/" + f.Name())
-            // log.Println("/dev/" + f.Name())
-            n++
-        }
-    }
+	contents, _ := ioutil.ReadDir("/dev")
+	n := 0
+	var duinos []string
+	// Look for what is mostly likely the Arduino device
+	for _, f := range contents {
+		if strings.Contains(f.Name(), "tty.usbserial") ||
+			strings.Contains(f.Name(), "ttyUSB") {
+			duinos = append(duinos, "/dev/"+f.Name())
+			// log.Println("/dev/" + f.Name())
+			n++
+		}
+	}
 
-    if(n != 0) {
-        return duinos, n, nil
-    }
+	if n != 0 {
+		return duinos, n, nil
+	}
 
-    // Have not been able to find a USB device that 'looks'
-    // like an Arduino.
-    return duinos, n, errors.New("Device Find: Unable to find HES")
+	// Have not been able to find a USB device that 'looks'
+	// like an Arduino.
+	return duinos, n, errors.New("Device Find: Unable to find HES")
 }
 
 // readProfile unmarshalls the json containing keybind information for HES
 func readProfile(jsonStr []byte) ([]keybinding, error) {
-    kbds := []keybinding{}
-    var data map[string][]json.RawMessage
-    err := json.Unmarshal(jsonStr, &data)
-    if err != nil {
-        log.Println(err)
-        return nil, errors.New("Unable to Unmarshal json")
-    }
-    for _,profile := range data["keybindings"] {
-        kbds = addKeybinding(profile, kbds)
-    }
-    return kbds, nil
+	kbds := []keybinding{}
+	var data map[string][]json.RawMessage
+	err := json.Unmarshal(jsonStr, &data)
+	if err != nil {
+		log.Println(err)
+		return nil, errors.New("Unable to Unmarshal json")
+	}
+	for _, profile := range data["keybindings"] {
+		kbds = addKeybinding(profile, kbds)
+	}
+	return kbds, nil
 }
 
 // addKeybinding creates keybinding structs and appends them to a slice
 func addKeybinding(profile json.RawMessage, kbds []keybinding) []keybinding {
-    kb := keybinding{}
-    if err := json.Unmarshal(profile, &kb); err != nil {
-        log.Println(err)
-    } else {
-        if kb != *new(keybinding) {
-            kbds = append(kbds, kb)
-        }
-    }
-    return kbds
+	kb := keybinding{}
+	if err := json.Unmarshal(profile, &kb); err != nil {
+		log.Println(err)
+	} else {
+		if kb != *new(keybinding) {
+			kbds = append(kbds, kb)
+		}
+	}
+	return kbds
 }
 
 // translateKeybindings converts strings from keybinding struct to keybd identifiers
-func translateKeybindings(kb keybinding) ([8]int) {
-    keymap := map[string]int{
-        "space": keybd.VK_ENTER,
-        "a":     keybd.VK_A,
-        "b":     keybd.VK_B,
-        "esc":   keybd.VK_ESC,
-        "c":     keybd.VK_C,
-        "v":     keybd.VK_V,
-        "d":     keybd.VK_D,
-        "w":     keybd.VK_W,
-        "s":     keybd.VK_S,
-        "l":     keybd.VK_L,
-        "k":     keybd.VK_K,
-        "m":     keybd.VK_M,
-        "n":     keybd.VK_N,
-        "g":     keybd.VK_G,
-        "up":    keybd.VK_UP,
-        "right": keybd.VK_RIGHT,
-        "down":  keybd.VK_DOWN,
-        "left":  keybd.VK_LEFT,
-    }
+func translateKeybindings(kb keybinding) [8]int {
+	keymap := map[string]int{
+		"space": keybd.VK_ENTER,
+		"a":     keybd.VK_A,
+		"b":     keybd.VK_B,
+		"esc":   keybd.VK_ESC,
+		"c":     keybd.VK_C,
+		"v":     keybd.VK_V,
+		"d":     keybd.VK_D,
+		"w":     keybd.VK_W,
+		"s":     keybd.VK_S,
+		"l":     keybd.VK_L,
+		"k":     keybd.VK_K,
+		"m":     keybd.VK_M,
+		"n":     keybd.VK_N,
+		"g":     keybd.VK_G,
+		"up":    keybd.VK_UP,
+		"right": keybd.VK_RIGHT,
+		"down":  keybd.VK_DOWN,
+		"left":  keybd.VK_LEFT,
+	}
 
-    var kb_array [8]int
-    kb_array[0] = keymap[kb.Selk]
-    kb_array[1] = keymap[kb.Start]
-    kb_array[2] = keymap[kb.Up]
-    kb_array[3] = keymap[kb.Down]
-    kb_array[4] = keymap[kb.Left]
-    kb_array[5] = keymap[kb.Right]
-    kb_array[6] = keymap[kb.B_key]
-    kb_array[7] = keymap[kb.A_key]
+	var kbArray [8]int
+	kbArray[0] = keymap[kb.Selk]
+	kbArray[1] = keymap[kb.Start]
+	kbArray[2] = keymap[kb.Up]
+	kbArray[3] = keymap[kb.Down]
+	kbArray[4] = keymap[kb.Left]
+	kbArray[5] = keymap[kb.Right]
+	kbArray[6] = keymap[kb.BKey]
+	kbArray[7] = keymap[kb.AKey]
 
-    return kb_array
+	return kbArray
 }
 
 func main() {
-    var wg sync.WaitGroup
-    // Find the device that represents the arduino serial
-    // connection.
-    duinos, n, err := findArduino();
-    if err != nil {
-        log.Fatal(err)
-    }
+	var wg sync.WaitGroup
+	// Find the device that represents the arduino serial
+	// connection.
+	duinos, n, err := findArduino()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Read the whole file at once
-    // Should not be done but I'm feeling rather trusting of user input today
-    b, err := ioutil.ReadFile("mappings.json")
-    if err != nil {
-        panic(err)
-    }
+	// Read the whole file at once
+	// Should not be done but I'm feeling rather trusting of user input today
+	b, err := ioutil.ReadFile("mappings.json")
+	if err != nil {
+		panic(err)
+	}
 
-    kbds, err := readProfile(b)
-    if err != nil {
-        log.Fatal(err)
-    }
+	kbds, err := readProfile(b)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    for i:= 0; i < n; i++ {
-        //log.Printf("%d %s %+v\n", i, duinos[i], kbds[i])
-        wg.Add(1)
-        go driver.CreateController(duinos[i], translateKeybindings(kbds[i]), &wg)
-    }
-    wg.Wait()
+	for i := 0; i < n; i++ {
+		//log.Printf("%d %s %+v\n", i, duinos[i], kbds[i])
+		wg.Add(1)
+		go driver.CreateController(duinos[i], translateKeybindings(kbds[i]), &wg)
+	}
+	wg.Wait()
 }
-
