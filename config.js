5e14157005accda3157797b5547914ed6e91cd6b
Mrrvm <mrrvm@hotmail.com>
2016-09-17 20:22:23 +0000

    Moved config.js to static folder

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 
-$("input").mouseleave(function(){$("#image").attr("src", "static/hes.png");});
-$("#A").mouseover(function(){$("#image").attr("src", "static/hes_A.png");});
-$("#B").mouseover(function(){$("#image").attr("src", "static/hes_B.png");});
-$("#Left").mouseover(function(){$("#image").attr("src", "static/hes_Left.png");});
-$("#Right").mouseover(function(){$("#image").attr("src", "static/hes_Right.png");});
-$("#Up").mouseover(function(){$("#image").attr("src", "static/hes_Up.png");});
-$("#Down").mouseover(function(){$("#image").attr("src", "static/hes_Down.png");});
-$("#Select").mouseover(function(){$("#image").attr("src", "static/hes_Select.png");});
-$("#Start").mouseover(function(){$("#image").attr("src", "static/hes_Start.png");});
+$("input").mouseleave(function(){$("#image").attr("src", "hes.png");});
+$("#A").mouseover(function(){$("#image").attr("src", "hes_A.png");});
+$("#B").mouseover(function(){$("#image").attr("src", "hes_B.png");});
+$("#Left").mouseover(function(){$("#image").attr("src", "hes_Left.png");});
+$("#Right").mouseover(function(){$("#image").attr("src", "hes_Right.png");});
+$("#Up").mouseover(function(){$("#image").attr("src", "hes_Up.png");});
+$("#Down").mouseover(function(){$("#image").attr("src", "hes_Down.png");});
+$("#Select").mouseover(function(){$("#image").attr("src", "hes_Select.png");});
+$("#Start").mouseover(function(){$("#image").attr("src", "hes_Start.png");});
