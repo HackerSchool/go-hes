25ca1417659a3ff4a875cc1bc736cb71ff57d458
Mrrvm <mrrvm@hotmail.com>
2016-09-18 00:30:35 +0000

    GUI done

--- static/config.js
+++ static/config.js
@@ -1,10 +1,18 @@
+var selected_value = "#0";
+$("table:not("+selected_value+")").hide();
+
+$("#selector").change(function() {
+	selected_value = '#'+$("#selector").val();
+	$(selected_value).show();
+	$("table:not("+selected_value+")").hide();
+});
 
 $("input").mouseleave(function(){$("#image").attr("src", "static/hes.png");});
-$("#A").mouseover(function(){$("#image").attr("src", "static/hes_A.png");});
-$("#B").mouseover(function(){$("#image").attr("src", "static/hes_B.png");});
-$("#Left").mouseover(function(){$("#image").attr("src", "static/hes_Left.png");});
-$("#Right").mouseover(function(){$("#image").attr("src", "static/hes_Right.png");});
-$("#Up").mouseover(function(){$("#image").attr("src", "static/hes_Up.png");});
-$("#Down").mouseover(function(){$("#image").attr("src", "static/hes_Down.png");});
-$("#Select").mouseover(function(){$("#image").attr("src", "static/hes_Select.png");});
-$("#Start").mouseover(function(){$("#image").attr("src", "static/hes_Start.png");});
+$(".A").mouseover(function(){$("#image").attr("src", "static/hes_A.png");});
+$(".B").mouseover(function(){$("#image").attr("src", "static/hes_B.png");});
+$(".Left").mouseover(function(){$("#image").attr("src", "static/hes_Left.png");});
+$(".Right").mouseover(function(){$("#image").attr("src", "static/hes_Right.png");});
+$(".Up").mouseover(function(){$("#image").attr("src", "static/hes_Up.png");});
+$(".Down").mouseover(function(){$("#image").attr("src", "static/hes_Down.png");});
+$(".Select").mouseover(function(){$("#image").attr("src", "static/hes_Select.png");});
+$(".Start").mouseover(function(){$("#image").attr("src", "static/hes_Start.png");});
